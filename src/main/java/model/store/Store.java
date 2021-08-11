0acfc0450e1a0f125f6cd83572dee1aad10b882a
Steven <stevenhca12@gmail.com>
2021-08-11 16:12:52 -0300

    Atributos de las sucursales + constructor

--- src/main/java/model/store/Store.java
+++ src/main/java/model/store/Store.java
@@ -1,4 +1,24 @@
 package model.store;
 
+import model.item.Item;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Store {
+    private Integer id;
+    private String address;
+    private String manager;
+    private Integer telephone;
+    private Map<Item,Integer> stock;
+
+
+    public Store(Integer id, String address, String manager, Integer telephone){
+        this.id = id;
+        this.address = address;
+        this.manager = manager;
+        this.telephone = telephone;
+        this.stock = new HashMap<>();
+    }
+
 }
